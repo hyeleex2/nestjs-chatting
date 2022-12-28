b7096d1dc281d4526b8b71a0bbaa44d4b5187cc1
hl.lee@quintet.co.kr <hgoguma124@gmail.com>
2022-12-28 21:33:44 +0900

    hook 추가

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,13 +1,19 @@
+import { Logger } from '@nestjs/common';
 import {
   SubscribeMessage,
   WebSocketGateway,
   MessageBody,
   ConnectedSocket,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
@@ -16,5 +22,21 @@ export class ChatsGateway {
     socket.emit('hello_user', `hello_${username}`);
     // socket.id
     return 'hello world';
+  }
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+  afterInit() {
+    this.logger.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    // nsp : namespace
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected`);
   }
 }
